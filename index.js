d1254bee212daf5176bfa3892dcb62db22635cd2
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-07-01 20:32:20 +0000

    addition of fly over times and callback to both iss and index

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned IP:' , ip);
 });
 
-fetchCoordsByIP('142.127.234.35', (error, data) => {
+fetchCoordsByIP('142.127.234.35', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  console.log("It worked! Returned coordinates:", coords);
+});
+
+fetchISSFlyOverTimes({ latitude: 43.6644, longitude: -79.4195 }, (error, times) => {
   if (error) {
     console.log(error);
     return;
   }
-  console.log(data);
+  console.log(times);
 });
